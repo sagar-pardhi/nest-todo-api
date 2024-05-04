e4512c7dac8bc0d0c8280cfd7b83e765c252d983
sagar-pardhi <sgrpardhi007@gmail.com>
2024-05-04 12:11:04 +0530

    add validation

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
   readonly text: string;
+
+  @IsBoolean()
+  @IsOptional()
   readonly isCompleted: boolean;
 }
